f205ec30ccc1188b7e64ccc7593683526e83fe75
TreyPoynter <treypoynter@gmail.com>
2023-11-14 11:34:15 -0600

    hide collapse on Link click

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,52 +4,57 @@ import '../assets/js/nav.js'
 import { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
-export default function Navbar({isLoggedIn, handleLogout}) {
+export default function Navbar({ isLoggedIn, handleLogout }) {
+    const navLinks = document.getElementsByClassName('nav-link');
+    console.log(navLinks)
+    function collapseOnClick() {
+        const navbarNav = document.getElementById('navbarNav');
+        if (navbarNav.classList.contains('show')) {
+            navbarNav.classList.remove('show');
+        }
+    }
     useEffect(() => {
-        document.addEventListener('DOMContentLoaded', function () {
-            const navLinks = document.getElementsByClassName('nav-link');
-        
-            navLinks.forEach((link) => {
-                link.addEventListener('click', () => {
-                    const navbarNav = document.getElementById('navbarNav');
-                    console.log('dwdad')
-                    if (navbarNav.classList.contains('show')) {
-                        navbarNav.classList.remove('show');
-                    }
-                });
-            });
+        const links = document.querySelectorAll('.nav-link');
+
+        links.forEach((link) => {
+            link.addEventListener('click', collapseOnClick);
+
+            // Cleanup event listeners when the component unmounts
+            return () => {
+                link.removeEventListener('click', collapseOnClick);
+            };
         });
-    });
+    }, []);
 
     return (
         <>
             <nav id='navbar' className="navbar bg-body-tertiary d-flex" style={{ position: 'fixed', top: 0, width: '100%' }}>
                 <div className="container-fluid d-flex w-100" id='nav-container'>
                     <div className='w-10'>
-                    <button onClick={() => {
-                        document.getElementById('navbarNav').classList.add('slide-in-left');
-                    }} id='nav-btn' className="navbar-toggler justify-content-end d-flex"
-                        type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <button onClick={() => {
+                            document.getElementById('navbarNav').classList.add('slide-in-left');
+                        }} id='nav-btn' className="navbar-toggler justify-content-end d-flex"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
                     </div>
-                    
+
                     <div className="w-50 d-flex justify-content-center text-center">
                         <a id='nav-brand' className="display-6" href="#">Issue-Tracker</a>
                     </div>
                     {
                         isLoggedIn ?
-                        <div id='nav-user' className="w-10 justify-content-around d-flex align-items-center">
-                            <Link class="fa-solid fa-user fa-xl text-black" to='users/user'></Link>
-                            <Link type='button' onClick={() => {handleLogout();}} to='login'>Logout</Link>
-                        </div> :
-                        <div id='nav-user' className="w-10 justify-content-around d-flex">
-                            <Link to='register'>Register</Link>
-                            <Link to='login'>Login</Link>
-                        </div>
+                            <div id='nav-user' className="w-10 justify-content-around d-flex align-items-center">
+                                <Link class="fa-solid fa-user fa-xl text-black" to='users/user'></Link>
+                                <Link type='button' onClick={() => { handleLogout(); }} to='login'>Logout</Link>
+                            </div> :
+                            <div id='nav-user' className="w-10 justify-content-around d-flex">
+                                <Link to='register'>Register</Link>
+                                <Link to='login'>Login</Link>
+                            </div>
                     }
-                    
+
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul id='nav-list' className="navbar-nav">
                             <div id="navbar-row" className="row">
@@ -74,7 +79,6 @@ export default function Navbar({isLoggedIn, handleLogout}) {
                                             ) : (
                                                 <Link to='login' className="nav-link active">Login to Show all Bugs</Link>
                                             )}
-                                            
                                         </li>
                                     </div>
                                 </div>
