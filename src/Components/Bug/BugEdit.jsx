42586ae65d0f8d5c41720598e419c5a066f53a2c
TreyPoynter <treypoynter@gmail.com>
2023-12-04 08:58:48 -0600

    Update BugEdit.jsx

--- src/Components/Bug/BugEdit.jsx
+++ src/Components/Bug/BugEdit.jsx
@@ -89,10 +89,10 @@ export default function EditBug({auth, showSuccess, showError}) {
 			<Error message="Must be Logged in"/>
 		)
 	}
-	if (!loggedUser.role.includes('business analyst') || loggedUser._id != bug?.assignedInfo?.assignedToUserId) {
+	if (!loggedUser.role.includes('business analyst') && loggedUser._id != bug?.assignedInfo?.assignedToUserId) {
 		return(
 			<Error message="Must be a Business Analyst or be Assigned to edit"/>
-		)
+		);
 	}
 	return (
 		<>
