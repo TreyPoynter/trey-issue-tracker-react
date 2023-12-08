2e621f42b046cd0fe70f378153c319089c77dfd0
TreyPoynter <treypoynter@gmail.com>
2023-12-08 09:35:35 -0600

    changed style of bug cards

--- src/Components/Bug/BugListItem.jsx
+++ src/Components/Bug/BugListItem.jsx
@@ -1,41 +1,48 @@
 /* eslint-disable */
 import { Link, NavLink } from "react-router-dom"
+import '../../assets/css/infoCard.css'
 
 export default function BugListItem({ bug }) {
 	const user = JSON.parse(localStorage.getItem('user'));
+	function formatDate(dateCreated) {
+		const date = new Date(dateCreated);
+
+		const formattedDate = date.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit'
+		});
+		return formattedDate;
+	}
 	return (
-		<div className="col-lg-3 col-md-6 mb-3">
-			<div className="justify-content-center mb-4 text-decoration-none card p-1">
-				<Link to={`/bugs/${bug._id}`} className="text-black text-decoration-none">
-					<div className="">
-						<div className="d-flex justify-content-between">
-						<span className={`badge bg-${bug.classification.classifiedAs == 'approved' ? 'success' :
+		<div className="col-lg-4 col-md-6 mb-3">
+			<Link className="text-decoration-none" to={`/bugs/${bug._id}`}>
+				<div className="info-card d-flex flex-column pt-2">
+					<div className="d-flex justify-content-between mb-2">
+						<i className={`badge bg-${bug.classification.classifiedAs == 'approved' ? 'success' :
 							bug.classification.classifiedAs == 'unapproved' || bug.classification.classifiedAs == 'duplicate'
-								? 'danger' : 'warning'}`}>{bug.classification.classifiedAs}</span>
-						<span className={` badge bg-${!bug.closedInfo.closed ? 'success ' : 'danger'}`}>
-							 {!bug.closedInfo.closed ? 'open' : 'closed'}
-						</span>
-						</div>
-						<h3 className="text-center fs-5">{bug.title}</h3>
+								? 'danger' : 'warning'}`}>{bug.classification.classifiedAs}
+						</i>
+						<i className={`badge bg-${!bug.closedInfo.closed ? 'success ' : 'danger'}`}>
+							{!bug.closedInfo.closed ? 'open' : 'closed'}
+						</i>
 					</div>
-					<div className="card-body text-center">
-						<i className="fas mb-2 fa-solid fa-5x fa-spider"></i>
-						<h5>Assigned to : {bug?.assignedInfo?.assignedToName ? bug.assignedInfo.assignedToName : 'No One'}</h5>
+					<div className="info-card-image d-flex justify-content-center align-items-center">
+						<i class="fa-solid fa-spider"></i>
 					</div>
-				</Link>
-				<div className="card-footer d-flex justify-content-around">
-					{user &&
-						(user.role.includes('business analyst') ||
-						bug.createdBy.user_id == user._id) &&
-						<Link to={`/bugs/${bug._id}/edit`} className="btn btn-warning">Edit Bug</Link>
-					}
-					{user &&
-						(user.role.includes('business analyst', 'technical manager') ||
-						bug.createdBy.user_id == user._id) &&
-						<Link to={`/bugs/${bug._id}/reassign`} className="btn btn-reassign">Reassign Bug</Link>
-					}
+					<p className="info-card-title">{bug.title}</p>
+					<div className="flex-grow-1"></div> {/* This div will push the info-footers to the bottom */}
+					<div className="d-flex flex-column justify-content-end align-items-end">
+						<p className="info-footer"><i class="fa-solid fa-user-tag"></i> Assigned to <span className="by-name">
+							John Doe</span>
+						</p>
+						<p className="info-footer"><i class="fa-regular fa-clock"></i> Created by <span className="by-name">
+							{bug.createdBy.name}</span> on <span className="by-name">{formatDate(bug.dateCreated)}</span>
+						</p>
+					</div>
 				</div>
-			</div>
+			</Link>
 		</div>
+
 	)
 }
