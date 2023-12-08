f063d79816d2e9f35985d55a65f9b2375f93055e
TreyPoynter <treypoynter@gmail.com>
2023-12-08 10:20:00 -0600

    Update UserListItem.jsx

--- src/Components/User/UserListItem.jsx
+++ src/Components/User/UserListItem.jsx
@@ -3,26 +3,35 @@ import { Link } from "react-router-dom"
 
 export default function User({ user }) {
     const loggedUser = JSON.parse(localStorage.getItem('user'));
+    function formatDate(dateCreated) {
+		const date = new Date(dateCreated);
+
+		const formattedDate = date.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit'
+		});
+		return formattedDate;
+	}
     return (
-        <div className="col-lg-3 col-md-6 mb-3">
-            <div className="justify-content-center mb-4 text-decoration-none card p-1">
-                <Link to={`/users/${user._id}`} 
-                className="text-black text-decoration-none">
-                    <div className="text-decoration-none">
-                        <h3 className="text-center fs-5">{user.fullName}</h3>
-                    </div>
-                    <div className="card-body text-center">
-                        <i className={`fas mb-2 fa-solid fa-5x fa-user`}></i>
-                    </div>
-                </Link>
-                <div className="card-footer d-flex justify-content-around">
-                    {loggedUser &&
-                        (loggedUser.role.includes('technical manager') ||
-                        user._id == loggedUser._id) &&
-                        <Link to={`/users/${user._id}/edit`} className="btn btn-warning">Edit User</Link>
-                    }
-                </div>
-            </div>
-        </div>
+        <div className="col-lg-4 col-md-6 mb-3">
+			<Link className="text-decoration-none" to={`/users/${user._id}`}>
+				<div className="info-card d-flex flex-column pt-2">
+					<div className="info-card-image d-flex justify-content-center align-items-center">
+						<i class="fa-solid fa-user"></i>
+					</div>
+					<p className="info-card-title">{user.fullName}</p>
+					<div className="flex-grow-1"></div> {/* This div will push the info-footers to the bottom */}
+					<div className="d-flex flex-column justify-content-end align-items-end">
+						<p className="info-footer"><i class="fa-solid fa-scroll"></i> Roles : {user.role.join(', ')}<span className="by-name">
+							</span>
+						</p>
+						<p className="info-footer"><i class="fa-regular fa-clock"></i> Joined <span className="by-name">
+							</span> on <span className="by-name">{formatDate(user.creationDate)}</span>
+						</p>
+					</div>
+				</div>
+			</Link>
+		</div>
     )
 }
