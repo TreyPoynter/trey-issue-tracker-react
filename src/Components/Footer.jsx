74f6ec643d3bb8cf30c0c11d0bfde83c07475bed
TreyPoynter <treypoynter@gmail.com>
2023-12-09 14:52:02 -0600

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,8 @@ export default function Footer() {
     return (
         <>
             <footer className="bg-dark text-white p-2 fixed-bottom text-center">
-                &copy; Trey Poynter 2023
+                &copy; <a target="_blank" className="text-white text-decoration-none" href="https://github.com/TreyPoynter">
+                    Trey Poynter</a> 2023
             </footer>
         </>
     )
