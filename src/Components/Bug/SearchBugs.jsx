53080e76c282110593baef7da3eb49434126b94a
TreyPoynter <treypoynter@gmail.com>
2023-12-10 11:47:09 -0600

    Update SearchBugs.jsx

--- src/Components/Bug/SearchBugs.jsx
+++ src/Components/Bug/SearchBugs.jsx
@@ -8,19 +8,21 @@ export default function SearchBugs({ setBugs, page }) {
     const [search, setSearch] = useState('');
 
     const onFormSubmit = (search, sortBy) => {
-        axios.get(`${import.meta.env.VITE_API_URL}/api/bugs/list/`, {withCredentials:true, 
-            params:{keywords:search, sortBy:sort}})
-        .then(res => {
-            console.log(res)
-            if (res.data.length < 1) {
-                setBugs([]);
-                return;
-            }
-            setBugs(res.data);
+        axios.get(`${import.meta.env.VITE_API_URL}/api/bugs/list/`, {
+            withCredentials: true,
+            params: { keywords: search, sortBy: sort }
         })
-        .catch(err => {
-            console.log(err);
-        })
+            .then(res => {
+                console.log(res)
+                if (res.data.length < 1) {
+                    setBugs([]);
+                    return;
+                }
+                setBugs(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
     const handleSortChange = (event) => {
         const newValue = event.target.value;
@@ -37,23 +39,33 @@ export default function SearchBugs({ setBugs, page }) {
     }
 
     return (
-        <>
-            <form onSubmit={(evt) => {evt.preventDefault();
-             onFormSubmit(evt.target.search.value,evt.target.sortBy.value);}} 
-            className="container d-flex justify-content-center mb-4">
-                <div className="d-flex w-50">
+        <form
+            onSubmit={(evt) => {
+                evt.preventDefault();
+                onFormSubmit(evt.target.search.value, evt.target.sortBy.value);
+            }}
+            className="container mb-4"
+        >
+            <div className="row">
+                <div className="col-md-4 mb-3">
                     <div className="input-group">
-                        <input onKeyDown={(evt) => detectEnter(evt)} onChange={(e) => setSearch(e.target.value)}
-                            className="form-control" id="search"type="text" placeholder={`Search for bugs by keywords`} 
-                            aria-label="Search" />
+                        <input
+                            onKeyDown={(evt) => detectEnter(evt)}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="form-control"
+                            id="search"
+                            type="text"
+                            placeholder={`Search for bugs by keywords`}
+                            aria-label="Search"
+                        />
                         <button className="btn btn-primary input-group-append" type="submit">
                             Search <i className="fa-solid fa-magnifying-glass ms-2"></i>
                         </button>
                     </div>
                 </div>
-                <div className="w-25 ms-5">
-                    <select onChange={(e) => handleSortChange(e)} id="sortBy" className="form-select"
-                    aria-label="Default select example">
+                <div className="col-md-4 mb-3">
+                    <select onChange={(e) => handleSortChange(e)}
+                        id="sortBy" className="form-select" aria-label="Default select example">
                         <option selected defaultValue={true} hidden disabled value={''}>Sort by...</option>
                         <option value="newest">Newest</option>
                         <option value="oldest">Oldest</option>
@@ -63,7 +75,21 @@ export default function SearchBugs({ setBugs, page }) {
                         <option value="createdBy">Created By</option>
                     </select>
                 </div>
-            </form>
-        </>
+                <div className="col-md-4 mb-3">
+                    <select onChange={(e) => handleSortChange(e)}
+                        id="sortBy" className="form-select" aria-label="Default select example">
+                        <option selected defaultValue={true} hidden disabled value={''}>Filter by...</option>
+                        <option value="newest">Newest</option>
+                        <option value="oldest">Oldest</option>
+                        <option value="title">Title</option>
+                        <option value="classification">Classification</option>
+                        <option value="assignedTo">Assigned To</option>
+                        <option value="createdBy">Created By</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+
+
     )
 }
