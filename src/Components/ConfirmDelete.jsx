3b67122bbaff974e58dc78d30fb3df5c82bb8a4b
TreyPoynter <treypoynter@gmail.com>
2023-12-11 11:20:19 -0600

    Confirm Delete UI update

--- src/Components/ConfirmDelete.jsx
+++ src/Components/ConfirmDelete.jsx
@@ -11,55 +11,73 @@ export default function ConfirmDelete({ deleteWhat, handleCancel, obj, loggedUse
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     function showError(message) {
-		toast(message, { type: 'error', position: 'top-right' });
-	}
-	function showSuccess(message) {
-		toast(message, { type: 'success', position: 'top-right' });
-	}
+        toast(message, { type: 'error', position: 'top-right' });
+    }
+    function showSuccess(message) {
+        toast(message, { type: 'success', position: 'top-right' });
+    }
     function handleDelete(evt) {
         setIsLoading(true);
         evt.preventDefault();
-        axios.delete(`${import.meta.env.VITE_API_URL}/api/${deleteWhat == 'User' ? 'users' : 'bugs'}/${
-            loggedUser._id == obj._id && deleteWhat == 'User' ? 'me' : obj._id}`,
-        {withCredentials:true}).then(res => {
-            setIsLoading(false);
-            showSuccess(`${deleteWhat == 'User' ? obj.fullName : obj.title} has been deleted`);
-            if (loggedUser._id == obj._id && deleteWhat == 'User') {
-                axios.post(`${import.meta.env.VITE_API_URL}/api/users/logout`,{},
-                {withCredentials: true}).then(res => {
-                    localStorage.removeItem('user');
-                    navigate('/');
-                    location.reload();
-                    return;
-                }).catch(err => {console.log(err)});
-            }
-            navigate(`/${deleteWhat == 'User' ? 'users' : 'bugs'}/list`);
-        }).catch(err => {
-            console.log(err)
-            showError(err.response.data.error)
-        });
+        axios.delete(`${import.meta.env.VITE_API_URL}/api/${deleteWhat == 'User' ? 'users' : 'bugs'}/${loggedUser._id == obj._id && deleteWhat == 'User' ? 'me' : obj._id}`,
+            { withCredentials: true }).then(res => {
+                setIsLoading(false);
+                showSuccess(`${deleteWhat == 'User' ? obj.fullName : obj.title} has been deleted`);
+                if (loggedUser._id == obj._id && deleteWhat == 'User') {
+                    axios.post(`${import.meta.env.VITE_API_URL}/api/users/logout`, {},
+                        { withCredentials: true }).then(res => {
+                            localStorage.removeItem('user');
+                            navigate('/');
+                            location.reload();
+                            return;
+                        }).catch(err => { console.log(err) });
+                }
+                navigate(`/${deleteWhat == 'User' ? 'users' : 'bugs'}/list`);
+            }).catch(err => {
+                console.log(err)
+                showError(err.response.data.error)
+            });
     }
 
     return (
         <>
             <div className="delete-container">
                 <div className="box">
-                    <h3>Delete {deleteWhat}?</h3>
-                    <p>Are you sure you want to delete</p>
+                    <h3 className="display-6">Delete {deleteWhat}?</h3>
+                    <p className="fs-5 m-0 text-center">Are you sure you want to delete</p>
 
                     <div className="warn_info">
                         <h4><i className="fa fa-warning"></i> Warning</h4>
-                        <p>By deleting {deleteWhat=='User' ? obj.fullName :
+                        <p>By deleting {deleteWhat == 'User' ? obj.fullName :
                             obj.title} you can't undo this action.</p>
                     </div>
 
-                    <div className="clearfix btns d-flex justify-content-center w-100">
-                        <button className="btn btn-secondary w-25 me-3" 
-                        type="button" disabled={isLoading} onClick={handleCancel}>Cancel</button>
-                        <button className="btn btn-danger w-25" onClick={(evt) => handleDelete(evt)} type="button"
-                        id="cancel" disabled={isLoading} href="">{isLoading ? 'Deleting...' : 'Delete'}
-                        {' ' + deleteWhat} <i className="fa fa-trash"></i></button>
+                    <div className="container mt-auto">
+                        <div className="row mb-4">
+                            <div className="col-md-6 col-12 mb-3">
+                                <button
+                                    className="btn btn-secondary w-100 text-center"
+                                    type="button"
+                                    disabled={isLoading}
+                                    onClick={handleCancel}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                            <div className="col-md-6 col-12">
+                                <button
+                                    className="btn btn-danger w-100"
+                                    onClick={(evt) => handleDelete(evt)}
+                                    type="button"
+                                    id="cancel"
+                                    disabled={isLoading}>
+                                    {isLoading ? 'Deleting...' : `Delete ${deleteWhat}`}
+                                    <i className="fa fa-trash"></i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </>
